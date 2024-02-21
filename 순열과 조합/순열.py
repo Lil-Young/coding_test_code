7bdb49cee2bee56e9e1e9730f4cda85cce030414
Lil-Young <thddyddls1@naver.com>
2024-02-21 23:52:49 +0900

    update

--- 순열과 조합/순열.py
+++ 순열과 조합/순열.py
@@ -1,4 +1,4 @@
 from itertools import permutations
 data = ['A', 'B', 'C'] # 데이터 준비
-result = list(permutations(data, 3))
-print(result)
+result = list(permutations(data, 3)) # 모든 순열 구하기
+print(result) # [(‘A', 'B', 'C’), (‘A', 'C', 'B’), (‘B', 'A', 'C’), (‘B', 'C', 'A’), (‘C', 'A', 'B’), (‘C', 'B', 'A’)]
